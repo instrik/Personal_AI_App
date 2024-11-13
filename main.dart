2154c00cf58b79183de067a5c6b4e0bcb0cee2a7
Aakash Agarwal <aakash@instrik.com>
2024-11-13 12:56:09 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -16,7 +16,7 @@ import 'screens/chat_screen.dart';
 void main() {
   runApp(MyApp());
 }
-//trial comment 5
+//trial comment 6
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
