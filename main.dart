8d17d19d133a217109d75a18c01707cb8fe88ed2
Aakash Agarwal <agarwal7409@gmail.com>
2026-06-12 23:58:37 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -12,7 +12,7 @@ import 'screens/chat_screen.dart';
  //    double eval = expression.evaluate(EvaluationType.REAL, contextModel);
  //    answer = eval.toString();
  //  }
-hj
+hjfy
 void main() {
   runApp(MyApp());
 }
